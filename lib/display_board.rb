f05c298aa583bbf753a4d6d314fd86933067f468
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-29 15:08:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,20 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  # puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  # puts "-----------"
+  # puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  # puts "-----------"
+  # puts " #{board[6]} | #{board[7]} | #{board[8]} "
   
+  cell_1 = [" #{board[0]} ", " #{board[1]} ", " #{board[2]} "]
+    cell_2 = [" #{board[3]} ", " #{board[4]} ", " #{board[5]} "]
+    cell_3 = [" #{board[6]} ", " #{board[7]} ", " #{board[8]} "]
+    sep = "-----------"
+    puts cell_1.join("|")
+    puts sep
+    puts cell_2.join("|")
+    puts sep
+    puts cell_3.join("|")
 end
 
- cell_1 = [" #{board[0]} ", " #{board[0]} ", " #{board[0]} "]
-  cell_2 = ["   ", "   ", "   "]
-  cell_3 = ["   ", "   ", "   "]
-  sep = "-----------"
-  puts cell_1.join("|")
-  puts sep
-  puts cell_2.join("|")
-  puts sep
-  puts cell_3.join("|")
+   
