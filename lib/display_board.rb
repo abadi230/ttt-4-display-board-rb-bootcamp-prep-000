f9a60c615cbd89d2f186d99255ef5bc589e50961
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-29 15:04:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,3 +8,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   
 end
+
+ cell_1 = [" #{board[0]} ", " #{board[0]} ", " #{board[0]} "]
+  cell_2 = ["   ", "   ", "   "]
+  cell_3 = ["   ", "   ", "   "]
+  sep = "-----------"
+  puts cell_1.join("|")
+  puts sep
+  puts cell_2.join("|")
+  puts sep
+  puts cell_3.join("|")
